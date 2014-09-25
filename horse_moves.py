d18489b86d5474ba5c127e3bb8c89bc16851f321
Kalle Hallivuori <kato@iki.fi>
2014-09-25 21:17:24 +0300

    Reordered source elements, removed letter handling methods, sort solutions in alphabetical order.

--- horse_moves.py
+++ horse_moves.py
@@ -1,5 +1,5 @@
 """
-Horse move computer
+Find out horse moves from start to target location on a chess board.
 
 Usage: python horse_moves.py [options] Xn-Ym ...
   Xn = start location (A1-H8)
@@ -13,38 +13,31 @@ import getopt
 import sys
 
 
-class ChessboardLocation:
-    def __init__(self, coordinates, previous_location=None):
-        self.x, self.y = isinstance(coordinates, list) and coordinates or [
-            (ord(coordinates[0]) - 65) & 7,
-            int(coordinates[1]) - 1
-        ]
-        self.previous_locations = previous_location and {str(previous_location): previous_location} or {}
+def usage():
+    print(__doc__.strip())
+    sys.exit(1)
 
-    def add_previous(self, previous_location):
-        self.previous_locations[str(previous_location)] = previous_location
 
-    def get_previous_locations(self):
-        return self.previous_locations.values()
+def report_solutions(start, finish, draw, verbose):
+    horse = HorseMoves(ChessboardLocation(start), ChessboardLocation(finish))
 
-    def set_letter(self, letter):
-        self.x = (ord(letter[0]) - 65)
+    if verbose:
+        horse.draw()
 
-    def get_letter(self):
-        return chr(65 + self.x)
+    horse.solve(callback=verbose and horse.draw or None)
 
-    def is_valid(self):
-        return (self.x == self.x & 7) and (self.y == self.y & 7)
+    if draw:
+        horse.draw()
 
-    def list_routes(self):
-        result = []
-        for previous in self.get_previous_locations():
-            for route in previous.list_routes():
-                result.append(route + [self])
-        return result or [[self]]
+    solution_strings = map(
+        lambda route: '-'.join([str(location) for location in route]),
+        horse.get_solutions()
+    )
+    solution_strings.sort()
 
-    def __str__(self):
-        return self.get_letter() + str(1 + self.y)
+    for solution_number in range(0, len(solution_strings)):
+        solution_string = solution_strings[solution_number]
+        print('%3d: %s' % (solution_number + 1, solution_string))
 
 
 class HorseMoves:
@@ -75,18 +68,19 @@ class HorseMoves:
         self.move_tree = pruned_move_tree
 
     def get_solutions(self):
+        assert self.solved()
         final_location = self.reached[self.finish_string]
         return final_location.list_routes()
 
     def find_shortest_solutions(self, callback=None):
         next_moves = self.move_tree[-1]
         while not self.solved():
-            next_moves, self.reached = self.possible_next_moves(next_moves, self.reached)
+            next_moves = self.possible_next_moves(next_moves)
             self.move_tree.append(next_moves)
             if callback:
                 callback()
 
-    def possible_next_moves(self, start_locations, reached):
+    def possible_next_moves(self, start_locations):
         next_moves = {}
 
         for start_location in start_locations:
@@ -100,7 +94,7 @@ class HorseMoves:
                         if new_location.is_valid():
                             new_location_string = str(new_location)
 
-                            if new_location_string in reached.keys():
+                            if new_location_string in self.reached.keys():
                                 pass # a shorter route there exists
                             elif new_location_string in next_moves.keys():
                                 # equally short route found
@@ -109,12 +103,13 @@ class HorseMoves:
                                 # first route there found
                                 next_moves[new_location_string] = new_location
 
-        reached.update(next_moves)
-        return next_moves.values(), reached
+        self.reached.update(next_moves)
+        return next_moves.values()
 
     def draw(self):
         chessboard = [[(((y ^ x) & 1) and '#' or '_') for x in range(0,8)] for y in range(0,8)]
         chessboard[self.finish_location.y][self.finish_location.x] = 'X'
+
         for move_index in range(0, len(self.move_tree)):
             for move in self.move_tree[move_index]:
                 chessboard[move.y][move.x] = '%d' % move_index
@@ -123,31 +118,48 @@ class HorseMoves:
         print('\n'.join(['%d %s' % (y + 1, ' '.join([chessboard[y][x] for x in range(0,8)])) for y in range(0,8)]))
 
 
-def report_solutions(start, finish, draw, verbose):
-    horse = HorseMoves(ChessboardLocation(start), ChessboardLocation(finish))
-    if verbose:
-        horse.draw()
-    horse.solve(callback=verbose and horse.draw or None)
-    if draw:
-        horse.draw()
-    solutions = horse.get_solutions()
-    for route_number in range(0, len(solutions)):
-        route = solutions[route_number]
-        print('%3d: %s' % (route_number + 1, ' '.join([str(location) for location in route])))
+class ChessboardLocation:
+    def __init__(self, coordinates, previous_location=None):
+        """
+        Please check validity with is_valid() after construction
 
+        :param coordinates: string "A1"-"H8" or list [x, y]
+        :param previous_location: another location that lead us here
+        """
 
-def usage():
-    print(__doc__.strip())
-    sys.exit(1)
+        self.x, self.y = isinstance(coordinates, list) and coordinates or [
+            ord(coordinates[0]) - 65,  # A-H -> 0-7 (or invalid value)
+            int(coordinates[1]) - 1    # 1-8 -> 0-7 (or invalid value)
+        ]
+        self.previous_locations = previous_location and {str(previous_location): previous_location} or {}
+
+    def is_valid(self):
+        return (self.x == self.x & 7) and (self.y == self.y & 7)
+
+    def add_previous(self, previous_location):
+        self.previous_locations[str(previous_location)] = previous_location
+
+    def get_previous_locations(self):
+        return self.previous_locations.values()
+
+    def list_routes(self):
+        result = []
+        for previous in self.get_previous_locations():
+            for route in previous.list_routes():
+                result.append(route + [self])
+        return result or [[self]]
+
+    def __str__(self):
+        return chr(65 + self.x) + str(1 + self.y)
+
 
 if __name__ == '__main__':
-    args = sys.argv[1:]
-    options_list, desires = getopt.getopt(args, '', ['draw', 'verbose', 'help'])
+    options_list, route_requests = getopt.getopt(sys.argv[1:], '', ['draw', 'verbose', 'help'])
     options = map(lambda pair: pair[0][2:], options_list)
 
-    if 'help' in options or not desires:
+    if 'help' in options or not route_requests:
         usage()
 
-    for desire in desires:
-        print(desire)
-        report_solutions(*desire.split('-'), draw='draw' in options, verbose='verbose' in options)
+    for route_request in route_requests:
+        print(route_request)
+        report_solutions(*route_request.split('-'), draw='draw' in options, verbose='verbose' in options)
